2e07dafac798d1b059555ae224e416c6c3843e89
lovenoel <kinglovenoel@gmail.com>
2024-10-21 03:12:15 +0000

    Corrections

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,10 +14,10 @@ def minOperations(n: int) -> int:
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     # Start checking for factors starting from 2
     while n > 1:
         # If n is divisible by factor, it's a valid step
@@ -26,5 +26,5 @@ def minOperations(n: int) -> int:
             operations += factor
             n //= factor  # Divide n by the factor
         factor += 1  # Move to the next potential factor
-    
+
     return operations
