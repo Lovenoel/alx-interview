691c4bd9674a37f12fb7d5d98add4579256d70da
lovenoel <kinglovenoel@gmail.com>
2024-10-21 03:10:07 +0000

    tasks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python3
+"""
+a method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file
+Returns an integer
+If n is impossible to achieve, return 0
+"""
+
+
 def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed to
+    result in exactly n H characters
+    """
     if n <= 1:
         return 0
     
@@ -9,7 +22,8 @@ def minOperations(n: int) -> int:
     while n > 1:
         # If n is divisible by factor, it's a valid step
         while n % factor == 0:
-            operations += factor  # Add the factor to the operation count
+            # Add the factor to the operation count
+            operations += factor
             n //= factor  # Divide n by the factor
         factor += 1  # Move to the next potential factor
     
